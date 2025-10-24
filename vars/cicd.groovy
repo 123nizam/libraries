9e63a9fe4590f2999692de60f5c65458b507ade8
Syed Nizam <nizam.vit@gmail.com>
2025-10-24 17:23:53 +0530

    Update cicd.groovy

--- vars/cicd.groovy
+++ vars/cicd.groovy
@@ -1,4 +1,20 @@
 def gitDownload(repo)
 {
-  git "https://github.com/IntelliqDevops/${repo}.git"
+   git "https://github.com/IntelliqDevops/${repo}.git"
+}
+
+def buildArtifact()
+{
+    sh 'mvn package'
+
+}
+
+def deployTomcat(ip,context)
+{
+    sh "scp /var/lib/jenkins/workspace/${JOB_NAME}/webapp/target/webapp.war ubuntu@${ip}:/var/lib/tomcat10/webapps/${context}.war"
+}
+
+def executeSelenium()
+{
+   sh "java -jar /var/lib/jenkins/workspace/${JOB_NAME}/testing.jar"
 }
